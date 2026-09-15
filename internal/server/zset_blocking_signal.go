8a3bf289a85f2c9f786294ab081080a3f6b6859a
edijs1984 <46107922+edijs1984@users.noreply.github.com>
2026-09-15 22:15:16 +0300

    Wake blocking ZSET waiters after committed writes

---
+++ internal/server/zset_blocking_signal.go
@@ -0,0 +1,28 @@
+package server
+
+import "strings"
+
+func (s *Server) signalZSetAvailability(args [][]byte, response []byte) {
+	if len(args) == 0 {
+		return
+	}
+	cmd := strings.ToUpper(string(args[0]))
+	switch cmd {
+	case "ZADD", "ZINCRBY":
+		if len(args) > 1 {
+			s.signalZSetKey(string(args[1]))
+		}
+	case "ZUNIONSTORE", "ZINTERSTORE", "ZDIFFSTORE", "ZRANGESTORE":
+		if len(args) > 1 {
+			s.signalZSetKey(string(args[1]))
+		}
+	case "RENAME":
+		if len(args) > 2 && strings.HasPrefix(string(response), "+OK") {
+			s.signalZSetKey(string(args[2]))
+		}
+	case "RENAMENX":
+		if len(args) > 2 && string(response) == ":1\r\n" {
+			s.signalZSetKey(string(args[2]))
+		}
+	}
+}
