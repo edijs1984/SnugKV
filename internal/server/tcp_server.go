3065099a55fbc2b0b8cc40439466eae1bbe99341
edijs1984 <46107922+edijs1984@users.noreply.github.com>
2026-09-15 22:32:41 +0300

    Preserve WRONGTYPE error prefix

--- internal/server/tcp_server.go
+++ internal/server/tcp_server.go
@@ -192,7 +192,7 @@ func (s *TCPServer) write(conn net.Conn, response []byte) error {
 func errorResponse(err error) []byte {
 	message := strings.TrimSpace(err.Error())
 	message = strings.NewReplacer("\r", " ", "\n", " ").Replace(message)
-	if !strings.HasPrefix(message, "ERR ") && !strings.HasPrefix(message, "NOPROTO ") && !strings.HasPrefix(message, "OOM ") {
+	if !strings.HasPrefix(message, "ERR ") && !strings.HasPrefix(message, "NOPROTO ") && !strings.HasPrefix(message, "OOM ") && !strings.HasPrefix(message, "WRONGTYPE ") {
 		message = "ERR " + message
 	}
 	return []byte("-" + message + "\r\n")
