13c4564b549ddb0b7b35c9a9c6a3621efc0133b0
edijs1984 <46107922+edijs1984@users.noreply.github.com>
2026-09-16 13:48:18 +0300

    Route XGROUP commands

--- internal/server/pressure.go
+++ internal/server/pressure.go
@@ -14,6 +14,9 @@ func (s *Server) executePressureCommand(args [][]byte) ([]byte, error) {
 	if isStreamCommand(args) {
 		return s.executeStream(args)
 	}
+	if isStreamGroupCommand(args) {
+		return s.executeStreamGroup(args)
+	}
 	if response, handled, err := s.executeStreamRename(args); handled {
 		return response, err
 	}
