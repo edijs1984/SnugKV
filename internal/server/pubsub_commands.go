555c4587b1ee4d4864347cebbca8cf61d2833c99
edijs1984 <46107922+edijs1984@users.noreply.github.com>
2026-09-16 16:24:52 +0300

    Add PubSub server commands

---
+++ internal/server/pubsub_commands.go
@@ -0,0 +1,104 @@
+package server
+
+import (
+	"errors"
+	"strings"
+)
+
+var pubSubCommands = map[string]commandInfo{
+	"PUBLISH":      {3, 3, 0, 0, 0, false},
+	"PUBSUB":       {2, 0, 0, 0, 0, false},
+	"SUBSCRIBE":    {2, 0, 0, 0, 0, false},
+	"UNSUBSCRIBE":  {1, 0, 0, 0, 0, false},
+	"PSUBSCRIBE":   {2, 0, 0, 0, 0, false},
+	"PUNSUBSCRIBE": {1, 0, 0, 0, 0, false},
+	"RESET":        {1, 1, 0, 0, 0, false},
+}
+
+func init() {
+	for name, info := range pubSubCommands {
+		commandTable[name] = info
+	}
+}
+
+func isPubSubServerCommand(args [][]byte) bool {
+	if len(args) == 0 {
+		return false
+	}
+	switch strings.ToUpper(string(args[0])) {
+	case "PUBLISH", "PUBSUB":
+		return true
+	default:
+		return false
+	}
+}
+
+func (s *Server) executePubSubServer(args [][]byte) ([]byte, error) {
+	if len(args) == 0 {
+		return nil, errors.New("ERR empty command")
+	}
+	cmd := strings.ToUpper(string(args[0]))
+	switch cmd {
+	case "PUBLISH":
+		if len(args) != 3 {
+			return nil, errors.New("ERR wrong number of arguments for 'publish' command")
+		}
+		return integer(pubSubHubForServer(s).publish(args[1], args[2])), nil
+
+	case "PUBSUB":
+		if len(args) < 2 {
+			return nil, errors.New("ERR wrong number of arguments for 'pubsub' command")
+		}
+		hub := pubSubHubForServer(s)
+		subcommand := strings.ToUpper(string(args[1]))
+		switch subcommand {
+		case "CHANNELS":
+			if len(args) != 2 && len(args) != 3 {
+				return nil, errors.New("ERR wrong number of arguments for 'pubsub|channels' command")
+			}
+			var pattern []byte
+			hasPattern := len(args) == 3
+			if hasPattern {
+				pattern = args[2]
+			}
+			channels := hub.channelsMatching(pattern, hasPattern)
+			items := make([][]byte, 0, len(channels))
+			for _, channel := range channels {
+				items = append(items, formatBulkString(channel))
+			}
+			return array(items...), nil
+
+		case "NUMSUB":
+			items := make([][]byte, 0, (len(args)-2)*2)
+			for _, channel := range args[2:] {
+				items = append(items, formatBulkString(channel), integer(hub.subscriberCount(channel)))
+			}
+			return array(items...), nil
+
+		case "NUMPAT":
+			if len(args) != 2 {
+				return nil, errors.New("ERR wrong number of arguments for 'pubsub|numpat' command")
+			}
+			return integer(hub.patternCount()), nil
+
+		case "HELP":
+			if len(args) != 2 {
+				return nil, errors.New("ERR wrong number of arguments for 'pubsub|help' command")
+			}
+			return array(
+				formatBulkString([]byte("PUBSUB <subcommand> [<arg> [value] [opt] ...]. Subcommands are:")),
+				formatBulkString([]byte("CHANNELS [<pattern>]")),
+				formatBulkString([]byte("    Return the currently active channels matching a pattern.")),
+				formatBulkString([]byte("NUMPAT")),
+				formatBulkString([]byte("    Return the number of active pattern subscriptions.")),
+				formatBulkString([]byte("NUMSUB [<channel> ...]")),
+				formatBulkString([]byte("    Return the number of subscribers for the specified channels.")),
+				formatBulkString([]byte("HELP")),
+				formatBulkString([]byte("    Prints this help.")),
+			), nil
+		default:
+			return nil, errors.New("ERR unknown subcommand")
+		}
+	}
+	return nil, errors.New("ERR unknown Pub/Sub command")
+}
