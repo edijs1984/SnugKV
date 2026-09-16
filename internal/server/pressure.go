c486afd866f43ef820782f288d1d3a4350aefe72
edijs1984 <46107922+edijs1984@users.noreply.github.com>
2026-09-16 10:57:34 +0300

    Route stream commands through pressure layer

--- internal/server/pressure.go
+++ internal/server/pressure.go
@@ -8,6 +8,12 @@ import (
 )
 
 func (s *Server) executePressureCommand(args [][]byte) ([]byte, error) {
+	if isStreamCommand(args) {
+		return s.executeStream(args)
+	}
+	if err := s.routeStreamRename(args); err != nil {
+		return nil, err
+	}
 	if isTypedScalarSpecial(args) {
 		return s.executeTypedScalarSpecial(args)
 	}
