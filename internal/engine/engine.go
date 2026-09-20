9f747a94ffd2e2edd0c722eecbedd3e03b4e1fbd
edijs1984 <46107922+edijs1984@users.noreply.github.com>
2026-09-20 12:44:19 +0300

    revert(engine): restore read activity index update

--- internal/engine/engine.go
+++ internal/engine/engine.go
@@ -157,6 +157,7 @@ func (s *Store) Get(key string) ([]byte, bool) {
 		if meta.reads < ^uint8(0) {
 			meta.reads++
 		}
+		sh.set(key, e)
 	}
 	return s.decode(sh, e), true
 }
@@ -212,6 +213,7 @@ func (s *Store) GetString(key string) (value []byte, found bool, wrongType bool)
 		if meta.reads < ^uint8(0) {
 			meta.reads++
 		}
+		sh.set(key, e)
 	}
 
 	return s.decode(sh, e), true, false
